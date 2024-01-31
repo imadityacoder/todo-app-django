22f8775a7046b6c712072dd781889151a03d3234
imadityacoder <iamadityacoder01@gmail.com>
2024-01-31 20:03:51 +0530

    create project

---
+++ Home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+]
